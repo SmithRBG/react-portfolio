ea9e2bb399168f4eec90ce2904289a17c8a98de4
SmithRBG <smit36000@gmail.com>
2021-09-26 09:13:09 -0400

    function issue

--- src/components/card.js
+++ src/components/card.js
@@ -2,15 +2,7 @@ import React from "react";
 
 function Card(props) {
   return (
-   /*  <div className="card text-center">
-      <div className="card-header">
-        <h2>Coding Quiz</h2>
-        </div>
-        <div>
-        <p class="card-text">A coding quiz showcasing javascript. Take the quiz and test your luck as a goth music expert.</p>
-      </div>
-      <div className="card-body">{props.children}</div>
-    </div> */
+   
     <div class="card">
     <img src="../assets/CQ1.jpg" class="card-img-top" alt="Coding Quiz Image" />
     <div class="card-body">
@@ -20,14 +12,14 @@ function Card(props) {
     </div>
     </div>
 
- /*    <div class="card">
+    <div class="card">
     <img src="" class="card-img-top" alt="API Search" />
     <div class="card-body">
     <h5 class="card-title">Ask Oracle</h5>
     <p class="card-text">A applicaion to look up superhero movies using an API.</p>
     <a href="https://devinpawline.github.io/Ask_Oracle/" class="btn btn-primary">Check it Out</a>
     </div>
-    </div> */
+    </div>
 
   );
 }
