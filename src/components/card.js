659d98d38d8430f5c145b032c571d1a8d8d55824
SmithRBG <smit36000@gmail.com>
2021-09-26 11:08:07 -0400

    rows

--- src/components/card.js
+++ src/components/card.js
@@ -1,10 +1,16 @@
 import React from "react";
 import Photo1 from "../assets/CQ1.jpg";
+import Photo2 from "../assets/photo2.JPG";
+import Photo3 from "../assets/photo3.JPG";
+import Photo4 from "../assets/photo4.JPG";
+import Photo5 from "../assets/photo5.JPG";
+import Photo6 from "../assets/photo6.JPG";
 
 function Card(props) {
   return (
    
 <>
+    <div class="bigBox">
             <div class="card cardcontainer">
             <img src={Photo1} class="card-img-top" alt="Coding Quiz Image" />
             <div class="card-body">
@@ -15,7 +21,7 @@ function Card(props) {
             </div>
 
             <div class="card cardcontainer">
-            <img src="" class="card-img-top" alt="API Search" />
+            <img src={Photo2} class="card-img-top" alt="API Search" />
             <div class="card-body">
             <h5 class="card-title">Ask Oracle</h5>
             <p class="card-text">A applicaion to look up superhero movies using an API.</p>
@@ -24,7 +30,7 @@ function Card(props) {
             </div>
 
             <div class="card cardcontainer">
-            <img src="" class="card-img-top" alt="Markdown Maker" />
+            <img src={Photo3} class="card-img-top" alt="Markdown Maker" />
             <div class="card-body">
             <h5 class="card-title">Markdown Maker</h5>
             <p class="card-text">This application utilizates Node to create a professional ReadMe.</p>
@@ -33,7 +39,7 @@ function Card(props) {
             </div>
 
             <div class="card cardcontainer">
-            <img src="" class="card-img-top" alt="Fitness Tracker" />
+            <img src={Photo4} class="card-img-top" alt="Fitness Tracker" />
             <div class="card-body">
             <h5 class="card-title">Fitness Tracker</h5>
             <p class="card-text">Keep track of your projects with ths fitness tracker. Uses MongoDB, Mongo Atlas, and deployed on Heroku.</p>
@@ -42,7 +48,7 @@ function Card(props) {
             </div>
 
             <div class="card cardcontainer">
-            <img src="" class="card-img-top" alt="GitShare Application" />
+            <img src={Photo5} class="card-img-top" alt="GitShare Application" />
             <div class="card-body">
             <h5 class="card-title">Git Share</h5>
             <p class="card-text">Get help with projects from around the globe, get started with GitShare. Git share uses Node, Express, and Sql.</p>
@@ -51,13 +57,14 @@ function Card(props) {
             </div>
 
             <div class="card cardcontainer">
-            <img src="" class="card-img-top" alt="E-Commerce Backend" />
+            <img src={Photo6} class="card-img-top" alt="E-Commerce Backend" />
             <div class="card-body">
             <h5 class="card-title">E-Commerce Backend</h5>
             <p class="card-text">E-Commerce backend configured with Express.js API using Sequelize to interact with a MySQL database.</p>
             <a href="https://github.com/SmithRBG/E-Commerce_Back_End" class="btn btn-primary">Check it Out</a>
             </div>
             </div>
+    </div>
 </>
 
   );
--- src/components/jumbotron.js
+++ src/components/jumbotron.js
@@ -5,7 +5,7 @@ return (
     <div class="p-5 mb-4 bg-light rounded-3">
     <div class="container-fluid py-5">
         <h1 class="display-5 fw-bold">Welcome!</h1>
-        <p class="col-md-8 fs-4">I'm a full-stack web developer, specializing in frontend development and UI/UX design.</p>
+        <p class="col-md-8 fs-4">I'm a full-stack web developer and freelance artist.</p>
     </div>
     </div>
     );
