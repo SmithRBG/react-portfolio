e55a0d95fd1ec62a5f80e4b126e044257f383492
SmithRBG <smit36000@gmail.com>
2021-09-26 09:12:28 -0400

    card function

--- src/components/card.js
+++ src/components/card.js
@@ -2,12 +2,33 @@ import React from "react";
 
 function Card(props) {
   return (
-    <div className="card text-center">
+   /*  <div className="card text-center">
       <div className="card-header">
-        <h2>{props.heading}</h2>
+        <h2>Coding Quiz</h2>
+        </div>
+        <div>
+        <p class="card-text">A coding quiz showcasing javascript. Take the quiz and test your luck as a goth music expert.</p>
       </div>
       <div className="card-body">{props.children}</div>
+    </div> */
+    <div class="card">
+    <img src="../assets/CQ1.jpg" class="card-img-top" alt="Coding Quiz Image" />
+    <div class="card-body">
+    <h5 class="card-title">Coding Quiz</h5>
+    <p class="card-text">A coding quiz showcasing javascript. Take the quiz and test your luck as a goth music expert.</p>
+    <a href="https://smithrbg.github.io/Coding-Quiz/" class="btn btn-primary">Check it Out</a>
     </div>
+    </div>
+
+ /*    <div class="card">
+    <img src="" class="card-img-top" alt="API Search" />
+    <div class="card-body">
+    <h5 class="card-title">Ask Oracle</h5>
+    <p class="card-text">A applicaion to look up superhero movies using an API.</p>
+    <a href="https://devinpawline.github.io/Ask_Oracle/" class="btn btn-primary">Check it Out</a>
+    </div>
+    </div> */
+
   );
 }
 
--- src/components/nav.js
+++ src/components/nav.js
@@ -5,7 +5,13 @@ function Navbar() {
   return (
     <ul class="nav justify-content-end">
   <li class="nav-item">
-    <a class="nav-link active" aria-current="page" href="/">Employee Directory</a>
+    <a class="nav-link active" aria-current="page" href="https://github.com/SmithRBG" target="_blank">GitHub</a>
+  </li>
+  <li class="nav-item">
+    <a class="nav-link active" aria-current="page" href="https://smithrbg.github.io/Developer-Website/" target="_blank">Website</a>
+  </li>
+  <li class="nav-item">
+    <a class="nav-link active" aria-current="page" href="https://docs.google.com/document/d/12d0QKOczpuSP1YJk_yjh15oIfCA0AnAXOmWYfsR5NFI/edit" target="_blank">Resume</a>
   </li>
   <li class="nav-item">
     <a class="nav-link disabled">Contact Me </a>
